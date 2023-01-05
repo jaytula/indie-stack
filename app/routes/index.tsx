4a28aa2299ee9aadde6b058b4457bd5a301dc2df
Jay T <jaytula@gmail.com>
2023-01-04 18:46:05 -0800

    Change dot to exclamation to test deploy

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,7 +24,7 @@ export default function Index() {
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
                 Check the README.md file for instructions on how to get this
-                project deployed.
+                project deployed!
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
